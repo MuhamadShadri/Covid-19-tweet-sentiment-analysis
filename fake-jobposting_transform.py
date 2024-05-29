4e69676aef337a617e4acb2b2323c4a135f852b3
MuhamadShadri <95609839+MuhamadShadri@users.noreply.github.com>
2024-05-30 02:31:18 +0700

    Add files via upload

---
+++ fake-jobposting_transform.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+FEATURE_KEY = "title"
+LABEL_KEY   = "label"
+
+
+# Renaming transformed features
+def transformed_name(key):
+    return key + "_xf"
+
+# Preprocess input features into transformed features
+def preprocessing_fn(inputs):
+    """
+    inputs:  map from feature keys to raw features
+    outputs: map from feature keys to transformed features
+    """
+
+    outputs = {}
+    outputs[transformed_name(FEATURE_KEY)] = tf.strings.lower(inputs[FEATURE_KEY])
+    outputs[transformed_name(LABEL_KEY)]   = tf.cast(inputs[LABEL_KEY], tf.int64)
+
+    return outputs
